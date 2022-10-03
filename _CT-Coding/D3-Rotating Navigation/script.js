f3a2582717593ff40e98330e31793ffa17841089
Cera Thomason <cetxw3@gmail.com>
2022-10-03 10:12:32 -0500

    added folders for each project

--- _CT-Coding/D3-Rotating Navigation/script.js
+++
@@ -1,7 +0,0 @@
-const open1 = document.getElementById('open')
-const close1 = document.getElementById('close')
-const container1 = document.querySelector('.container')
-
-open1.addEventListener('click', () => container1.classList.add('show-nav'))
-
-close1.addEventListener('click', () => container1.classList.remove('show-nav'))
